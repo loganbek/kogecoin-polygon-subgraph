68025390941a083531feeee8c841acf7de9f32c3
Logan Bek <loganbek@gmail.com>
2021-06-10 04:59:12 -0700

    schema + handlers

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,37 @@
 import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
 import { Gravatar } from '../generated/schema'
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+import {Approval, OwnershipTransferred, Transfer} from '../generated/Koge/Koge'
+
+// export function handleNewGravatar(event: NewGravatar): void {
+//   let gravatar = new Gravatar(event.params.id.toHex())
+//   gravatar.owner = event.params.owner
+//   gravatar.displayName = event.params.displayName
+//   gravatar.imageUrl = event.params.imageUrl
+//   gravatar.save()
+// }
+
+// export function handleUpdatedGravatar(event: UpdatedGravatar): void {
+//   let id = event.params.id.toHex()
+//   let gravatar = Gravatar.load(id)
+//   if (gravatar == null) {
+//     gravatar = new Gravatar(id)
+//   }
+//   gravatar.owner = event.params.owner
+//   gravatar.displayName = event.params.displayName
+//   gravatar.imageUrl = event.params.imageUrl
+//   gravatar.save()
+// }
+
+export function handleApproval(event:Approval):void {
+  
+  
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleOwnershipTransferred(event:OwnershipTransferred):void {
+
+}
+
+export function handleTransfer(event:Transfer):void {
+
 }

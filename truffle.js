59f29058c8b98fef3f72d093b5951e97d38cb88a
Logan Bek <loganbek@gmail.com>
2021-06-11 02:06:45 -0700

    truffle matic

--- truffle.js
+++ truffle.js
@@ -9,14 +9,12 @@ module.exports = {
       port: 8545,
       network_id: '*',
     },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(
-          process.env.MNEMONIC,
-          `https://ropsten.infura.io/v3/${process.env.ROPSTEN_INFURA_API_KEY}`
-        )
-      },
-      network_id: '3',
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mainnet.matic.network`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
     },
   },
   compilers: {

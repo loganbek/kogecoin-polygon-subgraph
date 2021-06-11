27abb45c76f0f02fce7a4d1b08e33b96f98b6dad
Logan Bek <loganbek@gmail.com>
2021-06-10 20:52:29 -0700

    handle transfer + transfer entitiy

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,7 +1,39 @@
 // import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
 // import { Transaction, Token, TokenBalance } from '../generated/schema'
 
-import {Approval, OwnershipTransferred, Transfer, balanceOf} from '../generated/Koge/Koge'
+import {Approval, OwnershipTransferred, balanceOf} from '../generated/Koge/Koge'
+import { Transfer as TransferEvent } from '../generated/Koge/Koge'
+// import { Transfer } from '../generated/schema'
+export function handleApproval(event:Approval):void {
+  
+}
+
+export function handleOwnershipTransferred(event:OwnershipTransferred):void {
+
+}
+
+//Handles new and already existing Transfer entities
+export function handleTransfer(event: TransferEvent): void {
+
+    
+    // Create a Transfer entity, using the hexadecimal string representation
+    // of the transaction hash as the entity ID
+    let id = event.transaction.hash.toHex()
+    let transfer = new Transfer(id)
+  
+    // Set properties on the entity, using the event parameters
+    transfer.from = event.params.from
+    transfer.to = event.params.to
+    transfer.amount = event.params.amount
+  
+    // Save the entity to the store
+    transfer.save()
+  }
+
+
+// export function handleBalanceOf(call:balanceOf):void {
+//     console.log(call.address)
+// }
 
 // export function handleNewGravatar(event: NewGravatar): void {
 //   let gravatar = new Gravatar(event.params.id.toHex())
@@ -21,20 +53,4 @@ import {Approval, OwnershipTransferred, Transfer, balanceOf} from '../generated/
 //   gravatar.displayName = event.params.displayName
 //   gravatar.imageUrl = event.params.imageUrl
 //   gravatar.save()
-// }
-
-export function handleApproval(event:Approval):void {
-  
-}
-
-export function handleOwnershipTransferred(event:OwnershipTransferred):void {
-
-}
-
-export function handleTransfer(event:Transfer):void {
-
-}
-
-// export function handleBalanceOf(call:balanceOf):void {
-//     console.log(call.address)
 // }
